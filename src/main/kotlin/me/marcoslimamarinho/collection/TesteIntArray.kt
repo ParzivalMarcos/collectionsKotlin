308254d58dce22741be20b9192ce0ba363d193ba
Marcos Lima Marinho <marcolim977@gmail.com>
2021-07-05 20:54:39 -0300

    Add function 'arrayInt' and 'arrayIntOf'

--- src/main/kotlin/me/marcoslimamarinho/collection/TesteIntArray.kt
+++ src/main/kotlin/me/marcoslimamarinho/collection/TesteIntArray.kt
@@ -1,7 +1,9 @@
 package me.marcoslimamarinho.collection
 
+const val SEPARADOR = "-----------------------"
 fun main() {
-    arrayInt()
+    //arrayInt()
+    arrayIntOf()
 }
 
 fun arrayInt(){
@@ -13,31 +15,46 @@ fun arrayInt(){
     values[3] = 10
     values[4] = 51
 
-    fun separador(){
-        println("-----------------------")
-    }
-
     // Laço comum
     for (valor in values){
         println(valor)
     }
 
-    separador()
+    println(SEPARADOR)
 
     // Expressões Lambdas
     values.forEach { valor ->
         println(valor)
     }
 
-    separador()
+    println(SEPARADOR)
 
     // Iterando sobre os índices do Array
     for (index in values.indices){
         println(values[index])
     }
 
-    separador()
+    println(SEPARADOR)
 
     values.sort()
     values.forEach { println(it) }
+
+    println(SEPARADOR)
 }
+
+fun arrayIntOf(){
+    val values = intArrayOf(2, 3, 1, 19, 14, 0)  // Inicializa passando os valores do array
+
+    values.forEach { println(it) }
+
+    println(SEPARADOR)
+
+    values.sort()
+    values.forEach { println(it) }
+
+
+
+
+}
+
+
